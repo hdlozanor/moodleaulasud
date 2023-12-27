7b9b534112f9936a849ab995bc6e9ba2a5e59795
hernanlozano93 <hdlozanor@udistrital.edu.co>
2023-12-27 13:38:36 -0500

    Carga Base 1
    Version 4.0.5
    
    27 Diciembre 2023

---
+++ config.php
@@ -0,0 +1,37 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'mysqli';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'aulasvirtuales.cwkcjtiatchy.us-east-1.rds.amazonaws.com';
+$CFG->dbname    = 'moodle_micro_aulas';
+$CFG->dbuser    = 'posgrado';
+$CFG->dbpass    = '!!p0sgr4d0s**';
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => 3306,
+  'dbsocket' => '',
+  'dbcollation' => 'utf8mb4_general_ci',
+);
+
+if (empty($_SERVER['HTTP_HOST'])) {
+  $_SERVER['HTTP_HOST'] = '127.0.0.1:8080';
+}
+if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') {
+  $CFG->wwwroot   = 'https://' . $_SERVER['HTTP_HOST'];
+} else {
+  $CFG->wwwroot   = 'http://' . $_SERVER['HTTP_HOST'];
+}
+$CFG->dataroot  = '/bitnami/moodledata';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 02775;
+
+require_once(__DIR__ . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
